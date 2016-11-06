b0b371e60a674a885bd0562610b07b282515a2c1
Sachin Grover <grover.sachin@gmail.com>
2016-11-06 17:55:19 +0530

    Updated the .gitignore - gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 const gulp = require('gulp');
-
 const eslint = require('gulp-eslint');
+const reporter = require('eslint-html-reporter');
 const htmlhint = require('gulp-htmlhint');
+
 const mocha = require('gulp-mocha');
 
 gulp.task('lint', ['eslint','htmlhint']);
 
 gulp.task('eslint', function() {
-  return gulp.src(['gulpfile.js','!node_modules/**/*'])
+  return gulp.src(['gulpfile.js','js/index.js','!node_modules/**/*'])
     .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    .pipe(eslint.format(reporter, function(results) {
+      fs.writeFileSync(path.join(__dirname, 'output/report-results.html'), results);
+    }))
+    .pipe(eslint.failAfterError())
 });
 
 gulp.task('htmlhint', function() {
   return gulp.src(['**/*.html','!node_modules/**/*'])
-  .pipe(htmlhint({htmlhintrc: ".htmlhintrc"}))
-  .pipe(htmlhint.failReporter());
+  .pipe(htmlhint({htmlhintrc: '.htmlhintrc'}))
+  .pipe(htmlhint.reporter('gulp-htmlhint-html-reporter', {
+     filename: __dirname + 'output/htmlhint-output.html',
+     createMissingFolders : true
+   }))
+  .pipe(htmlhint.failReporter())
 });
 
 gulp.task('test', function() {
